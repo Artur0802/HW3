71eae892e7133c3df7a10b750d86026b4e6710f8
Artur Ariukov <artur.ariukov@tietoevry.com>
2022-02-10 12:05:24 +0200

    Add project files.

---
+++ HW3/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW3
+{
+    class Person
+    {
+        private string name = "Name";
+        private string surname = "Surname";
+        private int age = 20;
+
+        public Person() { }
+
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine("Мне {0} лет, меня зовут {1} {2}.", age, name, surname);
+        }
+    }
+}
---
+++ HW3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+        }
+
+        public void PrintSum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void PrintMax(int a, int b)
+        {
+            Console.WriteLine(Math.Sqrt(Math.Max(a, b)));
+        }
+    }
+
+
+}
