0a396f787dc1e4e7b3e522f39471da62abd15ab8
Artur Ariukov <artur.ariukov@tietoevry.com>
2022-02-14 00:11:46 +0200

    HW4_done

---
+++ HW3/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW3
+{
+    public class Dog:Animal
+    {
+        private new string _sound = "Гав-Гав";
+
+        public Dog(int length, string name, string color)
+            : base(length, name, color)
+        {
+            if (length > 0)
+            {
+                this.length = length;
+            }
+            else
+            {
+                Console.WriteLine("Длина должна быть > 0");
+            }
+            this.name = name;
+            this.color = color;
+            base._sound = this._sound;
+        }
+    }
+}
--- HW3/Person.cs
+++ HW3/Person.cs
@@ -1,27 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HW3
 {
-    class Person
+    public class Person
     {
-        private string name = "Name";
-        private string surname = "Surname";
-        private int age = 20;
+        private string _name = "Имя";
+        private string _surname = "Фамилия";
+        private int _age = 20;
 
         public Person() { }
 
         public Person(string name, string surname, int age)
         {
-            this.name = name;
-            this.surname = surname;
-            this.age = age;
+            if (name.Length > 2)
+            {
+                _name = name;
+                _surname = surname;
+            }
+            else
+            {
+                Console.WriteLine("Имя должно быть больше 2 символов");
+                Console.ReadLine();
+            }
+
+            if (surname.Length > 2)
+            {
+                _surname = surname;
+            }
+            else
+            {
+                Console.WriteLine("Фамилия должна быть больше 2 символов");
+                Console.ReadLine();
+            }
+
+            if (age > 0 && age <= 120)
+            {
+                _age = age;
+            }
+            else
+            {
+                Console.WriteLine("Возраст должен быть больше 0 и не более 120 лет");
+                Console.ReadLine();
+            }
         }
 
         public void PrintPersonInfo()
         {
-            Console.WriteLine("Мне {0} лет, меня зовут {1} {2}.", age, name, surname);
+            Console.WriteLine("Мне {0} лет, меня зовут {1} {2}.", _age, _name, _surname);
+            Console.ReadLine();
         }
     }
 }
